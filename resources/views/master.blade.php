ccb9ce508384e63ef6fffc180378a209b6c50d36
mshibgah <muqoyyim@gmail.com>
2020-07-02 11:43:59 +0700

    summit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,56 @@
+@extends('template.index')
+
+@section('content')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Dashboard</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">dashboard</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header" style="background-color: orange">
+          <h3 class="card-title">Dashboard Admin</h3>
+
+          <div class="card-tools">
+            <a href="" class="btn btn-outline-primary rounded-pill" title="tambah data">
+              <i class="fas fa-plus"></i></a>
+          </div>
+        </div>
+        <div class="card-body">
+          <div class="alert" style="background-color: aqua;">
+            Selamat datang di Dashboard admin
+          </div>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+@endsection
+
